84878cf64cb3eabe05b4f42fa98e9cce45c3698b
Axel Krysztofiak <akrys@web.de>
2016-02-17 10:34:00 +0100

    Bugfix-Dokumentation

--- pages/release_notes/de/2016-02-14_1.0 Beta2.php
+++ pages/release_notes/de/2016-02-14_1.0 Beta2.php
@@ -2,5 +2,6 @@
 <ul>
 	<li>Erweiterung damit das Addon gleichzeitig in Redaxo 4 und Redaxo 5 funktioniert</li>
 	<li>Sprachdateien schon beim Installieren generieren (ISO bei Redaxo 4 UTF-8 bei Redaxo 5). Wenn das nicht geht, kann die Installation nicht abgeschlossen werden.</li>
+	<li>Bugfix: Im Template-Bereich wurde ein Template mehrfach verlinkt.</li>
 </ul>
 
--- pages/release_notes/en/2016-02-14_1.0 Beta2.php
+++ pages/release_notes/en/2016-02-14_1.0 Beta2.php
@@ -2,4 +2,5 @@
 <ul>
 	<li>Making this addon work in Redaxo 4 and Redaxo 5</li>
 	<li>Generate language files at install time. (ISO-8859-1 files in Redaxo 4; UTF-8 files in Redaxo 5). The install routine will not finish, if the language files cannot be created.</li>
+	<li>Bugfix: multiple template links in template section.</li>
 </ul>
