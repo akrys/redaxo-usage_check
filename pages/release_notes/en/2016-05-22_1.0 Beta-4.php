af0064aab0af799090a7cb41a30e76d9b2a505a4
Axel Krysztofiak <akrys@web.de>
2016-05-22 12:11:44 +0200

    v1.0-Beta4 fertig gestellt

--- pages/release_notes/en/2016-05-22_1.0 Beta-4.php
+++ pages/release_notes/en/2016-05-22_1.0 Beta-4.php
@@ -1,5 +1,6 @@
 
 <ul>
 	<li>Bug: files used only in article meta data are shown as used, even only unused should be shown</li>
+	<li>Some typo fixes. Thanks to alexschwarz89 and derincem</li>
 </ul>
 
