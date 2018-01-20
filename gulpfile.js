a7e2c5ba4f955433d5e59978b0f19b9c36402d48
Axel Krysztofiak <akrys@web.de>
2018-01-20 11:33:49 +0100

    composer & npm update

--- gulpfile.js
+++ gulpfile.js
@@ -119,11 +119,11 @@ gulp.task('pdepend', shell.task([
 //    gulp.watch(['phpunit.xml', './**/*.php', '!vendor/**/*', '!node_modules/**/*'], ['phplint']);
 //});
 
-gulp.task('default', ['phplint', 'phpcs', 'phpdoc', 'phpmd', 'pdepend', 'phpcpd', 'phpunit']);
-gulp.task('md', ['phpmd']);
-gulp.task('doc', ['phpdoc']);
-gulp.task('lint', ['phplint']);
-gulp.task('cs', ['phpcs']);
-gulp.task('depend', ['pdepend']);
-gulp.task('unit', ['phpunit']);
+gulp.task('default', gulp.parallel('phplint', 'phpcs', 'phpdoc', 'phpmd', 'pdepend', 'phpcpd', 'phpunit'));
+gulp.task('md', gulp.series('phpmd'));
+gulp.task('doc', gulp.series('phpdoc'));
+gulp.task('lint', gulp.series('phplint'));
+gulp.task('cs', gulp.series('phpcs'));
+gulp.task('depend', gulp.series('pdepend'));
+gulp.task('unit', gulp.series('phpunit'));
 
