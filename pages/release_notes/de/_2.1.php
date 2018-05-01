158f24ea5f9c9f2340a612782b2e360937e796ea
Axel Krysztofiak <akrys@web.de>
2018-05-01 15:46:00 +0200

    release notes

--- pages/release_notes/de/_2.1.php
+++ pages/release_notes/de/_2.1.php
@@ -2,4 +2,5 @@
 	<li>Refactoring: Funktionen, die sich auf XForm beziehen in YForm umbenannt</li>
 	<li>Textänderungen: Verweise auf XForm entfernt.</li>
 	<li>Aufräumaktion. Nicht benötigten Code entfernt</li>
+	<li>Bugfix: inatktive, genutzte Templates haben keinen Bearbeiten-Link</li>
 </ul>
--- pages/release_notes/en/_2.1.php
+++ pages/release_notes/en/_2.1.php
@@ -2,4 +2,5 @@
 	<li>Refactoring: renamed functions connectet with XForm to YForm</li>
 	<li>Text changes: removed connections to XForm.</li>
 	<li>Deleted unsued code</li>
+	<li>Bugfix: inactive but used templates don't have any edit link</li>
 </ul>
