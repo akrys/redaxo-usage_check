935c3bbca1c6304c837fd0417eca53e743fe6443
Axel Krysztofiak <akrys@web.de>
2023-11-04 18:24:20 +0100

    change log

--- pages/release_notes/de/_2.4.php
+++ pages/release_notes/de/_2.4.php
@@ -1,3 +1,5 @@
 <ul>
 	<li>Inline Styles eliminieren. Die CSS Datei nutzt nested css, was mit allen Browsern nach 2023-03 funktionieren sollte.</li>
+	<li>Issue 19: neue YForm-Typen <code>imagelist</code> & <code>custom_link</code></li>
+	<li>Issue 19: Bilder, die irgendwo in den value1 - value20 verlinkt sind. Damit dürften auch manuelle verlinkungen gefunden werden. Könnte auch fehleranfällig sein.</li>
 </ul>
