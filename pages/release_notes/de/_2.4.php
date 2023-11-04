f7730044610d3616436f36165010bbca3c0f40fb
Axel Krysztofiak <akrys@web.de>
2023-11-04 17:10:35 +0100

    changelog

---
+++ pages/release_notes/de/_2.4.php
@@ -0,0 +1,3 @@
+<ul>
+	<li>Inline Styles eliminieren. Die CSS Datei nutzt nested css, was mit allen Browsern nach 2023-03 funktionieren sollte.</li>
+</ul>
---
+++ pages/release_notes/en/_2.4.php
@@ -0,0 +1,3 @@
+<ul>
+	<li>eliminate inline styles. The CSS file uses nested css, which should be compatible with all current browsers realeased after 2023-03.</li>
+</ul>
