2883b60b9b22ee4d36bd74bd4091cd025814f07b
Axel Krysztofiak <akrys@web.de>
2023-11-06 19:53:24 +0100

    Release 2.4.1 fertig stellen fixes #21

---
+++ pages/release_notes/en/2023-11-06_2.4.1.php
@@ -0,0 +1,4 @@
+<ul>
+	<li>double entries due to sql fixes with `sql_mode=only_full_group_by`</li>
+	<li>Bugfix: unused templates were not found, because filter column was `0` instead of `null`</li>
+</ul>
