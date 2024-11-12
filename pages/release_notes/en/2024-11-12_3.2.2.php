e85bad5799f69b31927b014d002179d6870ece42
Axel Krysztofiak <akrys@web.de>
2024-11-12 22:10:02 +0100

    release notes

---
+++ pages/release_notes/en/2024-11-12_3.2.2.php
@@ -0,0 +1,13 @@
+
+<ul>
+	<li>
+		Adjustment in result sorting:
+		<table>
+			<tr><th>Pictures</th><td>By picture tiele, then by filename</td></tr>
+			<tr><th>Template</th><td>By template name</td></tr>
+			<tr><th>Module</th><td>By modul name</td></tr>
+			<tr><th>Actions</th><td>By action name</td></tr>
+		</table>
+	</li>
+	<li>Bugfix: Prevent error on detail page, if no result is present.</li>
+</ul>
